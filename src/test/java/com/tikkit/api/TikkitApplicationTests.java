a7e51cea5584a73d58c9a9d79eaa6ec1be12df40
flyingRKO <wogns970@naver.com>
2025-04-26 22:11:30 +0900

    Initial commit

---
+++ src/test/java/com/tikkit/api/TikkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikkit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
